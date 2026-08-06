df73cc73be296ac1c6c4e17b2be2725567dd714e
Yogeswaran S <yogeswaran3627@gmail.com>
2026-08-06 11:43:41 +0530

    fork()

---
+++ OS/t2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+int main() 
+{
+    pid_t pid = fork();
+
+    if (pid == 0){       
+       printf("P2: PID=%d Parent=%d\n", getpid(), getppid());
+       pid = fork();
+       if (pid == 0) 
+       {      
+	  printf("P3: PID=%d Parent=%d\n", getpid(), getppid());
+       }
+
+       else
+	  wait(NULL);
+    }
+
+    else if (pid > 0){               
+       printf("P1: PID=%d Parent=%d\n", getpid(), getppid());
+    }
+
+    else
+       wait(NULL);
+
+    return 0;
+}
